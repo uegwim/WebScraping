cb784390d7c02b7b97686af329dfd088e587a887
Ulunma Egwim <ulunmaegwim@gmail.com>
2019-10-20 16:29:04 -0400

    This is the code going through the list of our smaller seed set

---
+++ FinalScrape.py
@@ -0,0 +1,26 @@
+import bs4
+import urllib.request as urllib2
+import re
+import datetime
+
+seedset = ["http://toscrape.com/", "http://mentalfloss.com/article/53792/17-ancient-abandoned-websites-still-work",
+           "http://scratchpads.eu/explore/sites-list"]
+
+totallinks = []
+
+for url in seedset:
+    html_page = urllib2.urlopen(url)
+    soup = bs4.BeautifulSoup(html_page, features = "lxml")
+    links = []
+
+    for link in soup.findAll('a', attrs = {'href': re.compile("^http://")}):
+        links.append(link.get('href'))
+    totallinks += links
+
+i = 0
+for link in totallinks:
+    ts = datetime.datetime.now()
+    ts.strftime("%m/%d/%Y")
+    print(i, ".html", ts, link)
+    i+=1
+
